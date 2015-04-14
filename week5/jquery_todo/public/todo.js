06e574cc2fafdbdced86e393b3633060f95e8ea9
velichkoStoev <veli4kostoev@gmail.com>
2015-04-14 20:30:03 +0300

    TodoApp added

--- week5/jquery_todo/public/todo.js
+++ week5/jquery_todo/public/todo.js
@@ -1,8 +1,31 @@
-$( document ).ready(function() {
+function TodoApp(){
+    this.addTask = function(taskValue, container){
+        var task = taskValue;
+        var taskLabel = $('<label />').html(task);
+        var taskCheckbox = $('<input/>').attr({ type: 'checkbox' });
+        taskLabel.prepend(taskCheckbox);
+        container.append(taskLabel);
+        container.append('<br>');
+    },
+    this.completeTask = function(checkbox){
+        if(checkbox.is(':checked')){
+            checkbox.parent().wrap('<strike>');
+        }else{
+            checkbox.parent().unwrap('<strike>');
+        }
+    }
+}
+
+$(document).ready(function() {
+    var todoApp = new TodoApp();
+    var container = $('#tasks-field');  
+
     $('#add-btn').click(function(){
         var task = $('#task-input').val();
-        var taskCheckbox = $('<label />').html(task).prepend($('<input/>').attr({ type: 'checkbox'}));
-        $('#tasks-field').append(taskCheckbox);
-        $('#tasks-field').append('<br>');
+        todoApp.addTask(task, container);
+        
+        $('div#tasks-field label input').click(function(){
+            todoApp.completeTask($(this));
+        });
     });
 });
