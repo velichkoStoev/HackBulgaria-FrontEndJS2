3e0cbd119a58ef4d4b059d836baea08ecd955c9a
velichkoStoev <veli4kostoev@gmail.com>
2015-04-28 19:47:07 +0300

    rest_api

---
+++ week7/rest_api/index.js
@@ -0,0 +1,130 @@
+// load the express module
+var express = require('express');
+var bodyParser = require('body-parser');
+
+// declare our app
+var app = express();
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
+// this will serve as our resource on the server
+var students = [
+  {
+    id: '1', 
+    name: 'Kul Kul', 
+    email: 'kul@kul.com',
+    classes: ['Ailqk', 'Maina'],
+    gitRepo: 'kulcho' 
+  },
+  {
+    id: '2', 
+    name: 'Chill', 
+    email: 'chill@chill.com',
+    classes: ['Ailqk', 'Maina'],
+    gitRepo: 'chillcho'
+  }
+]
+
+// listen for requests
+var server = app.listen(1337, function() {
+ console.log('Listening on port %d', server.address().port);
+});
+
+//GET students
+app.get('/students', function(req, res){
+ res.jsonp(students);
+});
+
+// POST students
+app.post('/students', function(req, res){
+    var id = req.body.id;
+    var name = req.body.name;
+    var email = req.body.email;
+    var classes = req.body.classes;
+    var gitRepo = req.body.gitRepo;
+
+    var student = {
+        id: id,
+        name: name,
+        email: email,
+        classes: classes,
+        gitRepo: gitRepo
+    };
+
+    students.push(student);
+
+    res.jsonp({
+        msg: 'student created',
+        data: student
+    });
+});
+
+// GET students/:id
+app.get('/students/:id', function(req, res){
+    var id = req.params.id;
+    for(var i = 0; i < students.length; i+= 1){
+        if(students[i].id == id){
+            var student = students[i];
+            break;
+        }
+    }
+
+    res.jsonp({
+        msg: 'student with id of ' + id + ' returned',
+        data: student
+    });
+});
+
+// PUT students/:id
+app.put('/students/:id', function(req, res){
+    console.log(req.body);
+    var id = req.params.id;
+    
+    for(var i = 0; i < students.length; i+= 1){
+        if(students[i].id == id){
+            var student = students[i];
+            break;
+        }
+    }
+
+    student.name = req.body.name || student.name;
+    student.email = req.body.email || student.email;
+    student.gitRepo = req.body.gitRepo || student.gitRepo;
+    student.classes = req.body.classes || student.classes;
+
+    res.jsonp({
+        msg: 'student with id of ' + id + ' updated',
+        data: students[id]
+    });
+
+});
+
+// DELETE students/:id
+app.delete('/students/:id', function(req, res){
+    var id = req.params.id;
+    var msg;
+    var isDeleted = false;
+
+    for(var i = 0; i < students.length; i+= 1){
+        if(students[i].id == id){
+            delete students[id];
+            isDeleted = true;
+            break;
+        }
+    }
+
+    if(isDeleted){
+       delete students[id];
+       msg = 'student with id of ' + id + ' deleted';
+    }else{
+       msg = 'student with id of ' + id + ' does not exist';
+    }
+
+    res.jsonp({
+        msg: msg
+    });
+});
+
