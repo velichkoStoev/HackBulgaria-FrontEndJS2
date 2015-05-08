a6f01946acdd5b3dfc0e09f4985138f6a20473cf
velichkoStoev <veli4kostoev@gmail.com>
2015-05-08 20:33:22 +0300

    resource.js optimized

--- week8/promises/public/js/resource.js
+++ week8/promises/public/js/resource.js
@@ -3,38 +3,64 @@ function Resource(url){
 };
 
 Resource.prototype.query = function() {
-   return Q($.ajax({
-        url: this.url,
-        method: 'get'
-    }));
+   return Q($.get(this.url));
 };
 
 Resource.prototype.create = function(data){
     return Q($.ajax({
         url: this.url,
         method: 'post',
-        data: data
+        data: data,
+        dataType: 'json'
     }));
 }
 
 Resource.prototype.view = function(id){
-    return Q($.ajax({
-        url: this.url + '/id',
-        method: 'get'
-    }));
+    return Q($.get(this.url + id));
 }
 
 Resource.prototype.update = function(id, data){
     return Q($.ajax({
-        url: this.url + '/id',
+        url: this.url + id,
         method: 'put',
-        data: data
+        data: data,
+        dataType: 'json'
     }));  
 }
 
 Resource.prototype.delete = function(id){
     return Q($.ajax({
-        url: this.url + '/id',
+        url: this.url + id,
         method: 'delete'
     }));
 }
+
+var remoteServer = 'http://192.168.0.66:3000/api/students/';
+
+var res = new Resource(remoteServer);
+// res.query().then(function(data){
+//     console.log(data);
+// });
+
+//examples
+
+// res.create({
+//     email: 'bugav@abv.bg',
+//     name: 'mn bugav'
+// }).then(function(msg){
+//     console.log(msg);
+// });
+
+// res.view("554cea4fc23b62f14096f05b").then(function(data){
+//     console.log(data);
+// });
+
+// res.update("554cea4fc23b62f14096f05b", {
+//     name: 'kekeek'
+// }).then(function(data){
+//     console.log(data);
+// });
+
+// res.delete("554cea50c23b62f14096f05c").then(function(data){
+//     console.log(data);
+// });
